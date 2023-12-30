84e90f17737901c09919df2a1ecbab9075a18955
Juliasmatius <julinlenovo@gmail.com>
2023-12-30 11:02:00 +0200

    Create script.cmd

---
+++ script.cmd
@@ -0,0 +1,9 @@
+@ECHO OFF
+powershell -Command "Invoke-WebRequest https://www.nirsoft.net/utils/nircmd-x64.zip -OutFile nircmd.zip"
+powershell Expand-Archive nircmd.zip -DestinationPath .
+ nircmd.exe mutesysvolume 0
+explorer https://youtu.be/dQw4w9WgXcQ?si=_VXnCc8usNAB-fRi
+del nircmd.exe
+del nircmdc.exe
+del nircmd.zip
+start del script.cmd
