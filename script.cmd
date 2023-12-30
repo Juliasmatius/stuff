8f941a1524895cd1b9f6d407bd88c80ff23fc97c
Juliasmatius <julinlenovo@gmail.com>
2023-12-30 11:02:29 +0200

    Update script.cmd

--- script.cmd
+++ script.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 powershell -Command "Invoke-WebRequest https://www.nirsoft.net/utils/nircmd-x64.zip -OutFile nircmd.zip"
 powershell Expand-Archive nircmd.zip -DestinationPath .
- nircmd.exe mutesysvolume 0
+nircmd.exe mutesysvolume 0
 explorer https://youtu.be/dQw4w9WgXcQ?si=_VXnCc8usNAB-fRi
 del nircmd.exe
 del nircmdc.exe
